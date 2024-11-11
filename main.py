6fe4b5c3d69e4fb731a53ce39a68bcf1f7a2e6ca
ninja <astore.a.fabio@gmail.com>
2024-11-11 20:45:57 -0300

    page_redirect

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, status, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+REDIRECT_URL = ""
+
+
+@app.get("/", status_code=status.HTTP_200_OK, response_class=HTMLResponse)
+async def redirect_page(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "url": REDIRECT_URL
+        }
+    )
+
+
+@app.get("/{url}", status_code=status.HTTP_200_OK)
+async def set_redirect_page_url(url: str):
+    global REDIRECT_URL
+    REDIRECT_URL = "https://" + url + ".com"
+    return f"New url: {REDIRECT_URL}"
