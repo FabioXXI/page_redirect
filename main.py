ac7153d9945a130e5ed8e369481c1695b9edc010
ninja <astore.a.fabio@gmail.com>
2024-11-11 21:10:41 -0300

    page_redirect

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def set_redirect_page_url(url: str):
     return f"New url: {REDIRECT_URL}"
 
 
-@app.get("/zap", status_code=status.HTTP_200_OK)
+@app.get("/zap/set", status_code=status.HTTP_200_OK)
 async def set_zap_url():
     global REDIRECT_URL
     REDIRECT_URL = "https://api.whatsapp.com/send/?phone=5527995127940&text&type=phone_number&app_absent=0"
